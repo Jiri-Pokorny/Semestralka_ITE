b3ae6a70d1fe7b2e3aa75c48ed5a04db789eef72
Jiri-Pokorny <50661089+Jiri-Pokorny@users.noreply.github.com>
2022-10-24 09:40:58 +0200

    17.10. ESP8266 files
    Code for
    -getting temperature from the
     thermometer
    - connecting to the wifi

---
+++ wifi_connect.py
@@ -0,0 +1,21 @@
+import network
+sta_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
+
+SSID = 'IoT-CIV'
+key = 'UWBc9v.505.i0t'
+
+sta_if.active(True)
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, key)
+        while not sta_if.isconnected():
+            pass
+    print('Connected: ', sta_if.isconnected())
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
